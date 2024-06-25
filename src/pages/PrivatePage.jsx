a18d0820e177bddc86afd17f145b27ec87a6673b
Artem <79201625518qwe@gmail.com>
2024-06-25 11:38:56 +0300

    add mail

--- src/pages/PrivatePage.jsx
+++ src/pages/PrivatePage.jsx
@@ -2,15 +2,21 @@ import { useAuth } from "../hook/useAuth";
 import { useNavigate } from "react-router-dom";
 
 export const PrivatePage = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   const navigate = useNavigate();
   const handleSignOut = () => {
     signOut(() => navigate("/"), { replace: true });
   };
+  console.log(user);
   return (
     <>
       <div className="container">
         <h1>Private Page</h1>
+        <div>
+          <span>mail: </span>
+          <span>{user?.email}</span>{" "}
+        </div>
+
         <button onClick={handleSignOut}>Sign Out</button>
       </div>
     </>
